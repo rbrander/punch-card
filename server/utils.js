f488622b95b3a7322cd3bec4a9a6b74a385bacab
Joshua Smock <joshua@contentful.com>
2019-03-14 17:08:24 +0100

    Add reading/appending to data file

--- server/utils.js
+++ server/utils.js
@@ -1,9 +1,35 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const DATA_FILE = 'data.json';
+
+/*
+  Takes an object and appends it to the data file, and returns the object
+ */
 function appendData(obj) {
-  console.log(JSON.stringify(obj));
+  const data = readData();
+
+  data.push(obj);
+
+  writeFileSync(DATA_FILE, JSON.stringify(data));
+
+  return obj;
 }
 
+/*
+  Reads the current data in the data file and returns it as an object
+ */
 function readData() {
+  let raw;
 
+  try {
+    raw = readFileSync(DATA_FILE, 'utf8');
+  } catch (e) {
+    raw = "[]"
+  }
+
+  const parsed = JSON.parse(raw);
+
+  return parsed;
 }
 
 module.exports = {
