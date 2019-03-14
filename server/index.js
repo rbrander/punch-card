1c4382550e1151f186b60354052ef9a8e1357159
Joshua Smock <joshua@contentful.com>
2019-03-14 16:44:35 +0100

    Create basic server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const PORT = 8080;
+
+app.post('/time_entry', (req, resp, next) => {
+  resp.sendStatus(200);
+});
+
+app.get('/report', (req, resp, next) => {
+  resp.sendStatus(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Now listening on port ${PORT}`);
+})
