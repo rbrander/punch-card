0f8179946a97c9b8b9287e7410aa842603982bd9
Rob Brander <rob.brander@gmail.com>
2019-03-14 12:25:50 -0400

    add timestamp on post

--- server/index.js
+++ server/index.js
@@ -13,14 +13,14 @@ app.use(bodyParser.json());
 
 // Handler for accepting new time entry data
 app.post('/time_entry', (req, resp, next) => {
-  const timeEntry = new TimeEntry(req.body);
+  const timeEntryData = {...req.body, when: new Date().toISOString() };
+  const timeEntry = new TimeEntry(timeEntryData);
   appendData(timeEntry);
   resp.sendStatus(200);
 });
 
 app.get('/report', (req, resp, next) => {
   const data = readData();
-
   resp
     .status(200)
     .json(data);
