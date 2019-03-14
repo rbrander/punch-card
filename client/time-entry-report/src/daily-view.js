70608afecbffda43a9f128ac6d6b289177f05b84
Joshua Smock <joshua@contentful.com>
2019-03-14 20:29:02 +0100

    Add initial Daily component

--- client/time-entry-report/src/daily-view.js
+++ client/time-entry-report/src/daily-view.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import { Icon } from '@contentful/forma-36-react-components';
+import './Daily.css';
 
-const DailyView = () => (<h1>Daily View</h1>);
+const dates = [
+  {
+    id: '20190315',
+    name: 'March 15, 2019'
+  },
+  {
+    id: '20190314',
+    name: 'March 14, 2019'
+  },
+  {
+    id: '20190313',
+    name: 'March 13, 2019'
+  },
+  {
+    id: '20190312',
+    name: 'March 12, 2019'
+  },
+  {
+    id: '20190311',
+    name: 'March 11, 2019'
+  },
+  {
+    id: '20190310',
+    name: 'March 10, 2019'
+  },
+  {
+    id: '20190309',
+    name: 'March 9, 2019'
+  },
+]
 
-export default DailyView;
+export default class DailyView extends React.Component {
+  render() {
+    return (
+      <div>
+        { dates.map(date => (
+          <React.Fragment>
+            <div className='Daily__date' date={date.id}>
+              <Icon icon='ChevronRight' />
+              {date.name}
+            </div>
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  }
+}
